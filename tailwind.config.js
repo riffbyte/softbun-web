a655b3f165f002f8f9673f6f6867161bfed34641
Oleg Semyonov <oleg@prezly.com>
2022-01-22 21:27:33 +0300

    Setup initial styles and landing page stub

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-    content: [],
+    darkMode: 'class',
+    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                onyx: '#1c1d22',
+                aquamarine: '#47e5bc',
+                primary: '#47e5bc',
+                'mint-cream': '#eefcf8',
+                snow: '#fff2f1',
+            },
+        },
     },
     plugins: [],
 };
