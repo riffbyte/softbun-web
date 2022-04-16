9d609f365acedc9a6f80fa1719fbd7748154cb53
Oleg Semyonov <oleg@prezly.com>
2022-04-16 17:44:25 +0300

    Uncomment code that actually works now

--- lib/contentful/api/portfolio.ts
+++ lib/contentful/api/portfolio.ts
@@ -20,35 +20,34 @@ interface GetPortfolioItemsOptions {
 }
 
 export class PortfolioApi extends BaseApi {
-    // TODO: Debug this
-    // protected ENTRY_BODY_FIELDS = gql`
-    //     fragment EntryBodyFields on PortfolioItem {
-    //         body {
-    //             json
-    //             links {
-    //                 entries {
-    //                     inline {
-    //                         sys {
-    //                             id
-    //                         }
-    //                         __typename
-    //                     }
-    //                     block {
-    //                         sys {
-    //                             id
-    //                         }
-    //                         __typename
-    //                     }
-    //                 }
-    //                 assets {
-    //                     block {
-    //                         ${this.ASSET_FIELDS}
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // `;
+    protected ENTRY_BODY_FIELDS_FRAGMENT = gql`
+        fragment EntryBodyFields on PortfolioItem {
+            body {
+                json
+                links {
+                    entries {
+                        inline {
+                            sys {
+                                id
+                            }
+                            __typename
+                        }
+                        block {
+                            sys {
+                                id
+                            }
+                            __typename
+                        }
+                    }
+                    assets {
+                        block {
+                            ...AssetFields
+                        }
+                    }
+                }
+            }
+        }
+    `;
 
     async getPortfolioItems({ featured }: GetPortfolioItemsOptions = {}) {
         const whereClause = featured ? '{ featured: true }' : '{ featured_not: true }';
