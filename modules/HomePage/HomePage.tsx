a9a23919864f7a3505e8d05e15638edb975835a2
Oleg Semyonov <oleg@prezly.com>
2022-05-28 13:10:17 +0400

    Add more text to the homepage

--- modules/HomePage/HomePage.tsx
+++ modules/HomePage/HomePage.tsx
@@ -20,8 +20,7 @@ export function HomePage({ featuredPortfolioItems, otherPortfolioItems, careerIt
                 </h2>
 
                 <p>
-                    I&apos;m a full-stack software engineer developing great stuff for web and
-                    mobile.
+                    I&apos;m a full-stack web developer, making great stuff happen since 2012.
                     <br />
                     Currently building awesome tools for developers at{' '}
                     <a
@@ -35,12 +34,29 @@ export function HomePage({ featuredPortfolioItems, otherPortfolioItems, careerIt
                     .
                 </p>
             </Section>
-            <Section title="A little about me">
-                <p>Hey! My name is Oleg. TODO: I will add more info here.</p>
+            <Section title="A little about me" prose>
+                <p>
+                    I've been sailing the web development seas ever since my student years more than
+                    10 years ago. I've been creating everything from regular websites and
+                    dashboards, all the way to mobile applications and open-source libraries.
+                </p>
 
-                <p>TODO: A couple sentences about my work experience</p>
+                <p>
+                    The main characteristic of my work one could give for all these years has been
+                    simple:
+                </p>
 
-                <p>TODO: A little bit about my tech stack</p>
+                <blockquote className="text-2xl font-bold not-italic my-6 border-aquamarine">
+                    Get 💩 done. Fast.
+                </blockquote>
+
+                <p>
+                    I write clean and modern JS/TS code with a rigid code-style (man do I love
+                    ESLint!), and I do it <em className="font-bold">really fast</em>. Coming from
+                    the times when all we had was jQuery and plain CSS, I prefer to use the
+                    technologies as our web lords have intended, resulting in simple, maintainable
+                    and accessible applications.
+                </p>
             </Section>
             <Section title="My working career">
                 <ul>
