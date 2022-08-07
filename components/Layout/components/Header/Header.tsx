d8b5872561e5294825f3416b713d365b2bfb7b0a
Oleg Semyonov <oleg@prezly.com>
2022-08-07 15:56:30 +0400

    Add dark theme toggle

--- components/Layout/components/Header/Header.tsx
+++ components/Layout/components/Header/Header.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
+import { ThemeToggle } from './ThemeToggle';
+
 export function Header() {
     return (
         <header className="container py-6 lg:py-8 flex items-center justify-end space-x-6">
+            <ThemeToggle />
         </header>
     );
 }
