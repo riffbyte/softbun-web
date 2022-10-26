2c8a6c4498dc891d578e6ffdfa1c1bcedb07a3be
Oleg Semyonov <oleg@prezly.com>
2022-10-26 18:47:00 +0400

    Get rid of old /pages artifacts

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,7 @@ export { Button, ButtonLink } from './Button';
 export { CardGrid } from './CardGrid';
 export { Contacts } from './Contacts';
 export { Header } from './Header';
-export { Layout, Section } from './Layout';
 export { PortfolioCard } from './PortfolioCard';
 export { PortfolioLinks } from './PortfolioLinks';
 export { PortfolioTags } from './PortfolioTags';
+export { Section } from './Section';
